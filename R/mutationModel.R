c804f6dccfab53e75433f0ee0a6c438c29ed661c
magnusdv <magnusdv@medisin.uio.no>
2018-09-11 22:32:52 +0200

    Improve `mutationModel()` docs, checks and examples

--- R/mutationModel.R
+++ R/mutationModel.R
@@ -5,20 +5,20 @@
 #'
 #' @param model Either:
 #'
-#'   * a single `mutationMatrix`
+#'   * a `mutationModel` object (returned unchanged after validation)
 #'
-#'   * a list of two `mutationMatrix` objects, named "male" and "female"
+#'   * a single `mutationMatrix` object (will be applied to both genders)
 #'
-#'   * a single character string (see [mutationMatrix()] for valid options)
+#'   * a list of two `mutationMatrix` objects, named "female" and "male"
 #'
-#'   * a list of two strings, named (named "male" and "female")
+#'   * a single model name (see [mutationMatrix()] for valid options)
 #'
-#' @param matrix Either a single matrix, or a list of two (named "male" and
-#'   "female")
-#' @param rate Either a single number or a list of two (named "male" and
-#'   "female")
-#' @param seed Either a single integer or a list of two (named "male" and
-#'   "female")
+#'   * a list of two model names, named "female" and "male"
+#'
+#' @param matrix A matrix, or a list of two (named "female" and "male")
+#' @param rate A numeric mutation rate, or a list of two (named "female" and
+#'   "male")
+#' @param seed An integer, or a list of two (named "female" and "male")
 #' @param ... Further arguments to [mutationMatrix()], which are reused for both
 #'   models. Typically `alleles` and/or `afreq`.
 #' @param mutmod A `mutationModel` object
@@ -28,59 +28,79 @@
 #' @return An object of class `mutationModel`.
 #'
 #' @examples
-#' # Same model for both genders
+#' # "Equal" model, same parameters for both genders
 #' M1 = mutationModel("eq", alleles = 1:2, rate = 0.1)
 #' M1
-#' stopifnot(identical(M1$male, M1$female))
 #'
 #' # Different mutation rates
 #' M2 = mutationModel("eq", alleles = 1:2, rate = list(male = 0.1, female = 0.01))
 #' M2
-#' stopifnot(identical(M2$male, M1$male))
+#'
+#' stopifnot(identical(M1$male, M1$female), identical(M2$male, M1$male))
+#'
+#' # A custom mutation matrix:
+#' mat = matrix(c(0,0,1,1), ncol = 2, dimnames = list(1:2, 1:2))
+#' M3 = mutationModel(model = "custom", matrix = mat)
+#'
+#' # Under the hood arguments are passed to `mutationMatrix()`.
+#' # Alternatively, this can be done explicitly in the `model` argument
+#' M4 = mutationModel(model = mutationMatrix("custom", matrix = mat))
+#'
+#' stopifnot(identical(M3, M4))
+#'
+#' # The latter strategy is needed e.g. in pedtools::marker(), which gives the
+#' # user access to `model`, but not `matrix`.
 #'
 #' @export
 mutationModel = function(model, matrix = NULL, rate = NULL, seed = NULL, ...) {
-  # If "model" is a mutationMatrix:
-  # return mutationModel with this for both males and females
-  if(isMutationMatrix(model)) {
-    mod = structure(list(female = model, male = model),
-                    sexEqual = TRUE, class = "mutationModel")
-    return(validateMutationModel(mod))
+
+  if(isMutationModel(model)) {
+    return(validateMutationModel(model))
   }
 
-  # If "model" is list of two mutationMatrix objects:
-  # validate and return
-  if(is.list(model) && all(sapply(model, inherits, "mutationMatrix"))) {
-    stopifnot(length(model) == 2,
-              setequal(names(model), c("female", "male")))
-    mod = structure(model, sexEqual = identical(model$male, model$female),
-                    class = "mutationModel")
-    return(validateMutationModel(mod))
+  if(is.list(model) &&
+          length(model) == 2 &&
+          setequal(names(model), c("female", "male")) &&
+          all(sapply(model, isMutationMatrix))) {
+    mod = model
   }
+  else if(isMutationMatrix(model)) {
+    mod = list(female = model, male = model)
+  }
+  else if((is.character(model) && length(model) == 1) || is.list(model) && length(model) == 2) {
+    # Build models from parameters
+    model = validateSingleInput(model, "character")
+    matrix = validateMatrixInput(matrix)
+    rate = validateSingleInput(rate, "numeric")
+    seed = validateSingleInput(seed, "integer")
 
-  # Otherwise: build models from parameters
-  model = validateSingleInput(model, "character")
-  matrix = validateMatrixInput(matrix)
-  rate = validateSingleInput(rate, "numeric")
-  seed = validateSingleInput(seed, "integer")
+    female = mutationMatrix(model = model$female, matrix = matrix$female,
+                            rate = rate$female, seed = seed$female, ...)
 
-  female = mutationMatrix(model = model$female, matrix = matrix$female,
-                          rate = rate$female, seed = seed$female, ...)
+    male = mutationMatrix(model = model$male, matrix = matrix$male,
+                            rate = rate$male, seed = seed$male, ...)
 
-  male = mutationMatrix(model = model$male, matrix = matrix$male,
-                          rate = rate$male, seed = seed$male, ...)
+    mod = list(female = female, male = male)
+  }
+  else
+    stop2("`model` must be either:\n",
+          " * an object of class `mutationModel`\n",
+          " * an object of class `mutationMatrix`` (applied to both genders)`\n",
+          " * a list of two `mutationMatrix` objects, named 'female' and 'male'\n",
+          " * a character string (see ?mutationModel for valid options)\n",
+          " * a list of two characters, named 'female' and 'male'")
 
-  mod = structure(list(female = female, male = male),
-                  sexEqual = identical(female, male),
-                  class = "mutationModel")
-  validateMutationModel(mod)
+  mutmod = structure(mod,
+                     sexEqual = identical(mod$female, mod$male),
+                     class = "mutationModel")
+  validateMutationModel(mutmod)
 }
 
 validateSingleInput = function(x, mode) {
   if(is.null(x))
     return(x)
 
-  if(is.vector(x, mode = mode))
+  if(is.vector(x, mode = mode) && length(x) == 1)
     return(list(female = x, male = x))
 
   if(is.list(x) && length(x) == 2 && setequal(names(x), c("female", "male")))
@@ -109,10 +129,10 @@ validateMatrixInput = function(x) {
 #' @rdname mutationModel
 #' @export
 validateMutationModel = function(mutmod, alleles = NULL) {
-
   stopifnot(is.list(mutmod),
             length(mutmod) == 2,
             setequal(names(mutmod), c("male", "female")),
+            identical(attr(mutmod, "sexEqual"), identical(mutmod$male, mutmod$female)),
             inherits(mutmod, "mutationModel"))
 
   validateMutationMatrix(mutmod$male, alleles = alleles)
@@ -135,4 +155,8 @@ print.mutationModel = function(x, ...) {
     print(x$male, ...)
   }
   invisible(x)
+}
+
+isMutationModel = function(x) {
+  class(x) ==  "mutationModel"
 }
