8534edf5a422891a7fa8ab6db931dd318f90475c
magnusdv <magnusdv@medisin.uio.no>
2018-10-17 14:15:49 +0200

    Add tests of stepwise mutation matrices

--- tests/testthat/test-mutationMatrix.R
+++ tests/testthat/test-mutationMatrix.R
@@ -31,6 +31,12 @@ test_that("bad inputs are caught", {
                "Allele frequencies must sum to 1 after rounding to 3 decimals")
   expect_error(mutationMatrix(model="prop", alleles=1:2, afreq=c(.5,.5), rate=2),
                "Impossible mutation matrix; try reducing `rate`")
+  expect_error(mutationMatrix(model="step", alleles=1:2, rate=1, rate2=0.5, range=1),
+               "The total mutation rate")
+  expect_error(mutationMatrix(model="step", alleles=1:2, rate=0, rate2=0),
+               "`range` cannot be NULL with the 'stepwise' model")
+  expect_error(mutationMatrix(model="step", alleles=1:2, rate=0, rate2=0, range=0),
+               "`range` must be a positive number: 0")
 })
 
 
@@ -78,12 +84,21 @@ test_that("trivial model works", {
   expect_equivalent(m, mutationMatrix(matrix = m))
 })
 
-test_that("stepwise model works", {
+test_that("trivial stepwise model is diagonal", {
   m = mutationMatrix(alleles = 1:3, model = "step",
-                     rate=0, rate2=0, range=0)
+                     rate=0, rate2=0, range=99)
 
   expect_silent(validateMutationMatrix(m, alleles = 1:3))
   expect_equivalent(m, mutationMatrix(matrix = diag(3), alleles = 1:3))
+})
 
-  expect_equivalent(m, mutationMatrix(matrix = m))
+test_that("non-trivial stepwise mutation matrix is correct", {
+  alleles = c(1, 1.5, 2,3)
+  rate = 0.6
+  rate2 = 0.3
+  range = 0.5
+  M = mutationMatrix("step", alleles=alleles, rate=rate, rate2=rate2, range = range)
+  expect_equivalent(unclass(M),
+                    matrix(c(.1,.1,.3,.2,.3,.7,.3,.3,.4,.1,.1,.4,.2,.1,.3,.1),
+                           ncol=4, dimnames = list(alleles, alleles)))
 })
