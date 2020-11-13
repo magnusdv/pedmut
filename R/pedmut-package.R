2388f4158b7f2e207f1fe993444838c158a3b216
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-11-13 14:27:26 +0100

    Add package doc
    * Closes #7

---
+++ R/pedmut-package.R
@@ -0,0 +1,13 @@
+#' pedmut: Mutation Models for Pedigree Likelihood Computations
+#'
+#' A collection of functions for modelling mutations in pedigrees with marker
+#' data, as used e.g. in likelihood computations with microsatellite data.
+#' Implemented models include proportional and stepwise models, as well as
+#' random models for experimental work, and custom models allowing the user to
+#' apply any valid mutation matrix. Allele lumping is done following the
+#' lumpability criteria of Kemeny and Snell (1976), ISBN:0387901922.
+#'
+#' @docType package
+#'
+#' @name pedmut
+NULL
