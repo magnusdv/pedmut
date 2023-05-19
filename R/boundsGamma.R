82099c60350f6ff5f4b9c8527c11c3a22956092d
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-05-19 13:16:07 +0200

    Minor cleanups

--- R/boundsGamma.R
+++ R/boundsGamma.R
@@ -1,28 +1,28 @@
 #' Upper limits for overall mutation rate
-#'  
+#'
 #' @param alleles A character vector with allele labels.
 #' @param afreq A numeric vector of allele frequencies.
 #' @param range A positive number.
-#' 
+#'
 #' @return Bounds gamma for mutation matrix to be (i) well defined and (ii) regular.
-#' 
+#'
 #' @author Thore Egeland.
-#' 
+#'
 #' @export
-#' 
+#'
 #' @examples
 #' alleles = 1:3; afreq = c(0.2, 0.3,  0.5);  range = 0.1
 #' bounds = boundsGamma(alleles, afreq ,range)
 #' R1 = stepwiseReversible(alleles, afreq, rate = bounds[1], range)
-#' isRegular(R1, afreq)
+#' isBounded(R1, afreq)
 #' R2 = stepwiseReversible(alleles, afreq, rate = bounds[2], range)
-#' isRegular(R2, afreq)
-#'                         
+#' isBounded(R2, afreq)
+#'
 boundsGamma = function(alleles, afreq,  range){
   n = length(afreq)
-  
+
   R1 = matrix(ncol = n, nrow = n, 0)
-  
+
   for (i in 1:n){
     for(j in setdiff(1:n, i)){
       a = (1 - range^n)/(1 - range)
@@ -34,6 +34,6 @@ boundsGamma = function(alleles, afreq,  range){
   linesums = apply(R1, 1, sum)
   boundDefined = 1/max(linesums)
   maks = apply(R1, 2, max)
-  c(maxGammaDefined = boundDefined, 
+  c(maxGammaDefined = boundDefined,
     gammaRegular = min(afreq/maks))
 }
