fa91c1465f30c8015acc2fbda1465e135e63f4d5
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2026-04-18 17:23:08 +0200

    In `getParams()` add list output (format = 4)

--- R/getParams.R
+++ R/getParams.R
@@ -10,9 +10,8 @@
 #' @param sep A separator character used to paste male and female values.
 #'   Ignored unless `format = 3`.
 #'
-#' @returns When `mut` is a `mutationModel` with different male/female
-#'   parameters, the output format is dictated by the `format` option, with the
-#'   following possibilities:
+#' @returns When `mut` is a `mutationModel`, the output format is dictated by the `format`
+#'   option, with the following possibilities:
 #'
 #'   1. A data frame with 2 rows labelled 'female' and 'male'.
 #'
@@ -22,7 +21,9 @@
 #'   3. A data frame with 1 row, in which female/male values are pasted together
 #'   (separated with `sep`) if different.
 #'
-#'   If `mut` is a `mutationMatrix` the output always has 1 row.
+#'   4. A list version of format 1, suitable for programmatic use with `mutationModel()`.
+#'
+#'   If `mut` is a `mutationMatrix` the output is always a data frame with 1 row.
 #'
 #' @examples
 #' M = mutationModel("equal", 1:2, rate = list(female = 0.2, male = 0.1))
@@ -30,6 +31,11 @@
 #' getParams(M, format = 2)
 #' getParams(M, format = 3)
 #' getParams(M, format = 3, sep = "|")
+#'
+#' pars = getParams(M, format = 4)
+#' pars$alleles = 1:2  # Not part of the parameters, but needed to reconstruct the model.
+#' M2 = do.call(mutationModel, pars)
+#' stopifnot(identical(M, M2))
 #'
 #' @export
 getParams = function(mut, params = NULL, format = 1, sep = "/") {
@@ -39,25 +45,35 @@ getParams = function(mut, params = NULL, format = 1, sep = "/") {
     params = names(parlist$female)
 
     res = switch(format,
-           do.call(rbind, parlist),
-           {
-             names(parlist$female) = paste0(names(parlist$female), ".F")
-             names(parlist$male) = paste0(names(parlist$male), ".M")
-             res = cbind(parlist$female, parlist$male)
-             # Interleave
-             npar = length(params)
-             idx = as.numeric(rbind(1:npar, 1:npar + npar))
-             res[idx]
-           },
-           {
-             vals = lapply(params, function(p) {
-               valF = parlist$female[[p]]
-               valM = parlist$male[[p]]
-               if(identical(valF, valM)) valF else paste(valF, valM, sep = sep)
+            # 1
+            do.call(rbind, parlist),
+            # 2
+            {
+              names(parlist$female) = paste0(names(parlist$female), ".F")
+              names(parlist$male) = paste0(names(parlist$male), ".M")
+              res = cbind(parlist$female, parlist$male)
+              # Interleave
+              npar = length(params)
+              idx = as.numeric(rbind(1:npar, 1:npar + npar))
+              res[idx]
+            },
+            # 3
+            {
+              vals = lapply(params, function(p) {
+                valF = parlist$female[[p]]
+                valM = parlist$male[[p]]
+                if(identical(valF, valM)) valF else paste(valF, valM, sep = sep)
               })
-             names(vals) = params
-             as.data.frame(vals)
-           })
+              names(vals) = params
+              as.data.frame(vals)
+            },
+            # 4
+            {
+              vals = lapply(params, \(v) lapply(parlist, `[[`, v))
+              names(vals) = params
+              vals
+            }
+          )
     return(res)
   }
 
